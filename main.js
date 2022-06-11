bd6bfb2b72bcabff62d48ff27271591579243bd0
Souta10 <103153610+Souta10@users.noreply.github.com>
2022-06-11 11:11:08 +0900

    課題提出
    check1

---
+++ main.js
@@ -0,0 +1,17 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+if(numbers%2==0)
+function isEven() {
+    console.log(num + 'は偶数です');
+}
+
+class car{
+    constructor(name,numbers){
+        this,name=name;
+        this,numbers=numbers;
+    }
+}
+getNumgas(){
+    console.log(`ガソリンは${this.name}です。ナンバーは${this.numbers}です。`)
+}S
+let gasorin= new car(`レギュラー`,1234)
+getNumgas()
