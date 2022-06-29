143a7afd515ad11a81067a089917902b91e579c8
Souta10 <103153610+Souta10@users.noreply.github.com>
2022-06-30 07:52:22 +0900

    課題提出
    1章チェックテスト

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 let numbers = [2, 5, 12, 13, 15, 18, 22];
 for (let i=0; i<numbers.length; i++)
 if(numbers%2==0) 
-function isEven() {
     console.log(numbers + 'は偶数です');
-}
+
 
 class car{
     constructor(name,numbers){
         this.name=name;
         this.numbers=numbers;
-    }
-}
-
-getNumgas(){
-    console.log(`ガソリンは${this.name}です。ナンバーは${this.numbers}です。`);
-}
+   }
+   getNumgas(){
+        console.log(`ガソリンは${this.name}です。ナンバーは${this.numbers}です。`);
+  }
+} 
 let gasorin= new car(`レギュラー`,1234)
-getNumgas()
